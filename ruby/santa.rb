98d1f2e424321876311d5f14d394fcd5fd7efecf
jjnaidu <jnaidu@mail.sfsu.edu>
2016-11-12 17:37:56 -0800

    Refactor Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,8 @@
 class Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
 
 	def initialize(gender, ethnicity)
-		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", 
@@ -17,11 +18,32 @@ class Santa
 		puts "That was a good #{cookie}!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.push(reindeer)
+		p @reindeer_ranking
+	end
+
 end
 
+# Create Santa objects
 santas = []
 santas << Santa.new("male", "Latino")
 santas << Santa.new("female", "white")
 santas << Santa.new("agender", "black")
+
+# Test methods
 santas[0].speak
 santas[0].eat_milk_and_cookies("snickerdoodle")
+
+santas[1].celebrate_birthday
+puts "santas[1] age: #{santas[1].age}"
+puts "santas[1] ethnicity: #{santas[1].ethnicity}"
+
+santas[2].get_mad_at("Dancer")
+santas[2].gender = "gender fluid"
+puts "santas[2] gender: #{santas[2].gender}"
