aef73a509e5b6ddac85c224c07669737bccaa532
jjnaidu <jnaidu@mail.sfsu.edu>
2016-12-04 15:29:31 -0800

    Learn jQuery

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,9 +1,14 @@
+// When the egg image is clicked, it is replaced by an
+// image of a cracked open egg.
+
 function changeImage(event) {
 	event.target.src = "egg_cracked.jpg";
 }
 
 var photo = document.getElementById("egg-photo");
 photo.addEventListener("click", changeImage);
+
+// Text color is changed to red
 
 var text = document.getElementById("text");
 text.style.color = "red";
